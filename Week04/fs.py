b7d8999377e4e009d20037b938faeecf4fa0573b
champlainmorin <zachary.morin@mymail.champlain.edu>
2022-10-11 00:05:44 -0400

    Added Week 04 and Week 05

---
+++ Week04/fs.py
@@ -0,0 +1,101 @@
+import argparse
+import os
+import yaml
+import re
+from sre_constants import FAILURE
+class C:
+    """
+    A class that defines the ASCII control characters to color the terminal
+    """
+    HEADER = '\033[95m'
+    Yikes = '\033[94m'
+    Boomer = '\033[96m'
+    Forest = '\033[92m'
+    WARN = '\033[93m'
+    FAILURE = '\033[91m'
+    ENDCODE = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def load_files(directory):
+    # check if the given argument is a directory
+    if not os.path.isdir(directory):
+        print(f"Invalid Directory => {directory}")
+        exit()
+    f_list = []
+    # Crawl through the provided directory
+    for root, subfolders, filenames in os.walk(directory):
+        # loop through every file and add the root to it.
+        for f in filenames:
+            # file_list = directory + "\\" + f
+            file_list = directory + "/" + f
+            f_list.append(file_list)
+    return f_list
+
+def load_ruleset(ruleset):
+    rules = []
+    selected_ruleset = {}
+    with open("rules.yaml", 'r') as f:
+        try:
+            # Load all of the books within the yaml file
+            for rule in yaml.load_all(f, Loader=yaml.SafeLoader):
+                # Append the rules
+                rules.append(rule)
+        # Catch a yaml error if their is one.
+        except yaml.YAMLError as exc:
+            print(exc)
+    for rule in rules:
+        try:
+            if rule[ruleset]:
+                selected_ruleset = rule
+        except KeyError:
+            continue
+    # if the ruleset is validated return it
+    if selected_ruleset:
+        return selected_ruleset
+    # if not tell the user they entered an invalid ruleset.
+    else:
+        print(f"Invalid Ruleset => {ruleset}")
+        exit(0)
+        return None
+
+if __name__ == "__main__":
+    # set up our arg parser
+    parser = argparse.ArgumentParser(
+        description="Searches a group of log files for threat indicators based on a given ruleset",
+        epilog="Developed by Thomas Autiello 2022 02 10"
+    )
+    parser.add_argument("-d", "--directory", required=True, help="Directory with your logs.")
+    parser.add_argument("-r", "--rules", required=True, help="Ruleset from rules.yaml that you would like to use.")
+    args = parser.parse_args()
+    # load the values of our arguments into their respective variables
+    root_dir = args.directory
+    rule_set = args.rules
+    output_f = args.output
+    files = load_files(root_dir)
+    rules = load_ruleset(rule_set)
+    rule_def = rules[rule_set]
+    # create an empty list to store our output if it is going to be saved to a file
+    file_output_list = []
+    for file in files:
+        with open(file, "r") as f:
+            log_lines = f.readlines()
+            for log_line in log_lines:
+                for rule, definition in rule_def.items():
+                    # search the file for out match
+                    if re.search(definition, log_line):
+                        # if output file is defined, save the match to our output list
+                        if output_f:
+                            match = f"Rule: {rule} Has been matched in Ruleset: {rule_set} Within File: {file}\n" \
+                                    f"Matched Log Entry:{log_line}\n\n"
+                            file_output_list.append(match)
+                        # if no output is defined print the results to the console with color coding
+                        else:
+                            log_line = re.sub(definition, f"{C.FAILURE}{definition}{C.ENDCODE}", log_line)
+                            print(f"{C.WARN}Rule: {C.FAILURE}{rule}{C.WARN} Has been matched in Ruleset: {C.WARN}"
+                                  f"{rule_set} {C.WARN} Within File: {C.Boomer}{file}{C.ENDCODE}\n"
+                                  f"{C.Yikes}Matched Log Entry:{C.ENDCODES} {log_line}\n\n")
+    if output_f and match:
+        with open(output_f, "w+") as f:
+            for match in file_output_list:
+                f.write(match)
---
+++ Week05/urlHaus.py
@@ -0,0 +1,63 @@
+import csv
+import re
+
+
+# Renamed method to be urlHouse to remove the 1 that was stuck in there.
+def urlHaus(filename, searchTerm):
+
+    eachOccurence = []
+    try:
+        # Tabbed out each line individually, moved the print function to be within the for var loop.
+        with open(filename) as f:
+            contents = csv.reader(f)
+            # Parsing the CSV's rows
+            for skipped_line in range(9):
+                next(contents)
+
+                # Parsing the CSV's column values
+            for eachLine in contents:
+                for keyword in searchTerm:
+                    #print(searchTerm[0])
+                    for x in range(0, len(eachLine)):
+                        #print(searchTerm[0])
+                        #print("eachLine["+str(x)+"] = " + eachLine[x])
+                        if keyword in eachLine[x]:
+                            eachOccurence.append(eachLine)
+
+
+        
+        return eachOccurence
+    except:
+        pass
+
+"""
+                # Finding in the value the .tld of the value in this usecase.
+                x = re.findall(r"" + keyword, eachLine[3])
+                #print("The eachLine[2] is: "+eachLine[2])
+                if (keyword) in eachLine[2]:
+                    e = eachLine[2]
+                    #print("The x is: " + x)
+
+                # For the test dataset, set the_src to eachLine[6]
+                # For the actual dataset, set the_src to eachLine[7]
+                try: 
+                    if keyword in e:
+                        for var in x:
+                            
+                            # Makes the URL not clickable by replacing the dangerous URL's HTTP to HXXP.
+                            the_url = e.replace("http","hxxp") # Gets the value of the CSV in the 2nd index position.
+                            the_src = eachLine[7] # Gets the value of the CSV in the 6th index position.
+
+                            # Fixed the print function to just be an f-string, not as fancy but is effective.
+                            print(fURL: {the_url}
+Info: {the_src}
+*****************************************************************
+            
+                except:
+                    pass
+
+# Don't edit this line. It is here to show how it is possible
+# to remove the "tt" so programs don't convert the malicious
+# domains to links that an be accidentally clicked on.
+"""
+
