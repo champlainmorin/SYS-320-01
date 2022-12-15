626a9b8c489891b72e89e720b5de4b2ad4196bde
champlainmorin <zachary.morin@mymail.champlain.edu>
2022-12-15 17:13:12 -0500

    Updated weeks

--- Week12/Classwork/Threat.ps1
+++ Week12/Classwork/Threat.ps1
@@ -1,45 +1,40 @@
-# Array of websites containing threat intell
+# Websites with threat intel
 $drop_urls = @('https://rules.emergingthreats.net/blockrules/emerging-botcc.rules','https://rules.emergingthreats.net/blockrules/compromised-ips.txt')
 
 # Loop through the URLS for the rules list
-foreach ($u in $drop_urls) {
+foreach ($url in $drop_urls) {
 
     # Extract the filename
-    $temp = $u.split("/")
+    $temp = $url.split("/")
 
-    # The last element in the array plucked off is the filename
+    # The last item in the directory is the filename
     $file_name = $temp[4]
 
     if (Test-Path $file_name) {
-
         continue
 
     } else {
+        # Download the rules list from the website
+        Invoke-WebRequest -Uri $url -OutFile $file_name
 
-        # Download the rules list
-        Invoke-WebRequest -Uri $u -OutFile $file_name
+    } 
 
-    } # Close if statement
-
-} # Close the foreach loop
+}
 
 # Array containing the filename
 $input_paths = @('.\compromised-ips.txt','.\emerging-botcc.rules')
 
-# Extract the IP addresses.
+# Extract the IP's
 #108.190.109.107
 #108.191.2.72
 $regex_drop = '\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
 
-# Append the IP addresses to the temporary IP list.
+# Append the IP addresses according to the regex
 Select-String -Path $input_paths -Pattern $regex_drop | `
 ForEach-Object { $_.Matches } | `
 ForEach-Object { $_.Value } | Sort-Object | Get-Unique | `
 Out-File -FilePath "ips-bad.tmp"
 
-# Get the IP addresses discovered, loop through and replace the beginning of the line with th IPTables syntax
-# After the IP address, add the remaining IPTables syntax and save the results to a file.
+# Creates the bash file that has the syntax to drop IP's from iptables.
 # iptables -A INPUT -s 108.191.2.72 -j DROP
-(Get-Content -Path ".\ips-bad.tmp") | % `
-{ $_ -replace "^","iptables -A INPUT -s " -replace "$", "-j DROP" }
-Out-File -FilePath "iptables.bash"
+(Get-Content -Path ".\ips-bad.tmp") | ForEach-Object { $_ -replace "^","iptables -A INPUT -s " -replace "$", " -j DROP" } | Out-File -FilePath "iptables.bash"
