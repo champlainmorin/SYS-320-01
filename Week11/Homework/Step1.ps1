7d8099aee8bb81febcd9a1865c504f6f14f08cfa
Morin <zachary.morin@mymail.champlain.edu>
2022-12-15 15:01:06 -0500

    Updated Week11

--- Week11/Homework/Step1.ps1
+++ Week11/Homework/Step1.ps1
@@ -1,27 +1,26 @@
 # Task 1 Work
+# Copying the Powershell executable to my directory.
+Copy-Item "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Destination "C:\Users\zachary.morin"
 
-
-Copy-Item "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Destination "C:\Users\zach"
-
-# Creates a random number to go with the new file.
+# Creates a random number to append to the filename.
 $NumberG = Get-Random -Minimum 1000 -Maximum 9000
 $Random = "EnNoB-$NumberG.exe"
-Rename-Item -Path "C:\Users\zach\powershell.exe" -NewName $Random
+Rename-Item -Path "C:\Users\zachary.morin\powershell.exe" -NewName $Random
 
-# Statement will see if the file is there and if it is not, then it will print out error.
-if ( Test-Path "C:\Users\zach\$Random") {
-    Write-Host "File is Found!"
+# If statement will see if the file is there and if it is not, then it will say so.
+if ( Test-Path "C:\Users\zachary.morin\$Random") {
+    Write-Host "File is found!"
 } else {
-    Write-Host "error"
+    Write-Host "File not found."
 }
 
 
 
 # Task 2 Work
 $message = "If you want your files restored, please contact me at dunston@champlain.edu. I look forward to doing business with you."
-$message | Out-File -FilePath "C:\Users\zach\Desktop\Readme.READ"
+$message | Out-File -FilePath "C:\Users\zachary.morin\Desktop\Readme.READ"
 
-if ( Test-Path "C:\Users\zach\Desktop\Readme.READ" ){
+if ( Test-Path "C:\Users\zachary.morin\Desktop\Readme.READ" ){
     Write-Host "File is Found!"
 } else {
     Write-Host "Error"
